3afa3c9a74d0cf755e3c11adb68b97eab37d383e
hannasrnn <122799018+hannasrnn@users.noreply.github.com>
2023-06-03 14:28:17 +0200

    add 5 rounds

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -26,5 +26,105 @@ function gamePlay (playerSelection, getComputerChoice) {
     else return 'Try again'
 }
 
-const playerSelection = "rock";
+var playerSelection = "rock";
+console.log(gamePlay(playerSelection.toLowerCase(), getComputerChoice()));
+
+function gamePlay (playerSelection, getComputerChoice) {
+    if (playerSelection === "rock" && getComputerChoice=== "Paper")
+        return 'You lose! Paper beats rock.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Paper")
+    return 'You win! Scissors beats paper.'
+    if (playerSelection === "paper" && getComputerChoice=== "Paper")
+    return 'It\'s a tie.'
+    if (playerSelection === "rock" && getComputerChoice=== "Rock")
+        return 'It is a tie.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Rock")
+    return 'You lose! Rock beats scissors.'
+    if (playerSelection === "paper" && getComputerChoice=== "Rock")
+    return 'You win! Paper beats rock.'
+    if (playerSelection === "rock" && getComputerChoice=== "Scissors")
+        return 'You win! Rock beats scissors.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Scissors")
+    return 'Tie game.'
+    if (playerSelection === "paper" && getComputerChoice=== "Scissors")
+    return 'You lose! Scissors beats paper.'
+    else return 'Try again'
+}
+
+var playerSelection = "paper";
+console.log(gamePlay(playerSelection.toLowerCase(), getComputerChoice()));
+
+function gamePlay (playerSelection, getComputerChoice) {
+    if (playerSelection === "rock" && getComputerChoice=== "Paper")
+        return 'You lose! Paper beats rock.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Paper")
+    return 'You win! Scissors beats paper.'
+    if (playerSelection === "paper" && getComputerChoice=== "Paper")
+    return 'It\'s a tie.'
+    if (playerSelection === "rock" && getComputerChoice=== "Rock")
+        return 'It is a tie.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Rock")
+    return 'You lose! Rock beats scissors.'
+    if (playerSelection === "paper" && getComputerChoice=== "Rock")
+    return 'You win! Paper beats rock.'
+    if (playerSelection === "rock" && getComputerChoice=== "Scissors")
+        return 'You win! Rock beats scissors.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Scissors")
+    return 'Tie game.'
+    if (playerSelection === "paper" && getComputerChoice=== "Scissors")
+    return 'You lose! Scissors beats paper.'
+    else return 'Try again'
+}
+
+var playerSelection = "scissors";
+console.log(gamePlay(playerSelection.toLowerCase(), getComputerChoice()));
+
+function gamePlay (playerSelection, getComputerChoice) {
+    if (playerSelection === "rock" && getComputerChoice=== "Paper")
+        return 'You lose! Paper beats rock.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Paper")
+    return 'You win! Scissors beats paper.'
+    if (playerSelection === "paper" && getComputerChoice=== "Paper")
+    return 'It\'s a tie.'
+    if (playerSelection === "rock" && getComputerChoice=== "Rock")
+        return 'It is a tie.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Rock")
+    return 'You lose! Rock beats scissors.'
+    if (playerSelection === "paper" && getComputerChoice=== "Rock")
+    return 'You win! Paper beats rock.'
+    if (playerSelection === "rock" && getComputerChoice=== "Scissors")
+        return 'You win! Rock beats scissors.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Scissors")
+    return 'Tie game.'
+    if (playerSelection === "paper" && getComputerChoice=== "Scissors")
+    return 'You lose! Scissors beats paper.'
+    else return 'Try again'
+}
+
+var playerSelection = "rock";
+console.log(gamePlay(playerSelection.toLowerCase(), getComputerChoice()));
+
+function gamePlay (playerSelection, getComputerChoice) {
+    if (playerSelection === "rock" && getComputerChoice=== "Paper")
+        return 'You lose! Paper beats rock.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Paper")
+    return 'You win! Scissors beats paper.'
+    if (playerSelection === "paper" && getComputerChoice=== "Paper")
+    return 'It\'s a tie.'
+    if (playerSelection === "rock" && getComputerChoice=== "Rock")
+        return 'It is a tie.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Rock")
+    return 'You lose! Rock beats scissors.'
+    if (playerSelection === "paper" && getComputerChoice=== "Rock")
+    return 'You win! Paper beats rock.'
+    if (playerSelection === "rock" && getComputerChoice=== "Scissors")
+        return 'You win! Rock beats scissors.'
+    if (playerSelection === "scissors" && getComputerChoice=== "Scissors")
+    return 'Tie game.'
+    if (playerSelection === "paper" && getComputerChoice=== "Scissors")
+    return 'You lose! Scissors beats paper.'
+    else return 'Try again'
+}
+
+var playerSelection = "paper";
 console.log(gamePlay(playerSelection.toLowerCase(), getComputerChoice()));
