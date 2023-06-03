cd877105114148554584b5d6795c36e7ee69acd2
hannasrnn <122799018+hannasrnn@users.noreply.github.com>
2023-06-03 14:17:29 +0200

    add toLowerCase

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -9,33 +9,34 @@ function gamePlay (playerSelection, getComputerChoice) {
     if (playerSelection.toLowerCase === "rock" && getComputerChoice=== "Paper")
         return 'You lose! Paper beats rock.'
 
-    if (playerSelection.toLowerCase === "scissors" && getComputerChoice=== "Paper")
+    if (playerSelection === "scissors" && getComputerChoice=== "Paper")
     return 'You win! Scissors beats paper.'
 
-    if (playerSelection.toLowerCase === "paper" && getComputerChoice=== "Paper")
+    if (playerSelection === "paper" && getComputerChoice=== "Paper")
     return 'It\'s a tie.'
 
-    if (playerSelection.toLowerCase === "rock" && getComputerChoice=== "Rock")
+    if (playerSelection === "rock" && getComputerChoice=== "Rock")
         return 'It is a tie.'
 
-    if (playerSelection.toLowerCase === "scissors" && getComputerChoice=== "Rock")
+    if (playerSelection === "scissors" && getComputerChoice=== "Rock")
     return 'You lose! Rock beats scissors.'
 
-    if (playerSelection.toLowerCase === "paper" && getComputerChoice=== "Rock")
+    if (playerSelection === "paper" && getComputerChoice=== "Rock")
     return 'You win! Paper beats rock.'
 
-    if (playerSelection.toLowerCase === "rock" && getComputerChoice=== "Scissors")
+    if (playerSelection === "rock" && getComputerChoice=== "Scissors")
         return 'You win! Rock beats scissors.'
 
-    if (playerSelection.toLowerCase === "scissors" && getComputerChoice=== "Scissors")
+    if (playerSelection === "scissors" && getComputerChoice=== "Scissors")
     return 'Tie game.'
 
-    if (playerSelection.toLowerCase === "paper" && getComputerChoice=== "Scissors")
+    if (playerSelection === "paper" && getComputerChoice=== "Scissors")
     return 'You lose! Scissors beats paper.'
 
     else return 'Try again'
 }
 
-const playerSelection = "scissors"
 
-console.log(gamePlay(playerSelection, getComputerChoice));
+const playerSelection = "ScIssors";
+
+console.log(playerSelection.toLowerCase(), getComputerChoice());
