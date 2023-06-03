b8c318d30bde07d9ac564b2131774c29c05fb79c
hannasrnn <122799018+hannasrnn@users.noreply.github.com>
2023-06-03 13:02:30 +0200

    fix getComputerChoice

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,20 +1,42 @@
-const getComputerChoice = () => {
-    let choice = ["Rock", "Paper", "Scissors"];
-    let randomRPS = choice[Math.floor(Math.random()* choice.length)];
-    return randomRPS;
+
+    const choice = ["Rock", "Paper", "Scissors"];
+    
+    function getComputerChoice(){
+    return choice[Math.floor(Math.random()* choice.length)];
 }
 
-function gamePlay (playerSelection, getComputerChoice) {
-    if (playerSelection === "rock" && getComputerChoice=== "Paper")
-        return 'You lose! Paper beats rock.'
 
-    if (playerSelection === "scissors" && getComputerChoice=== "Paper")
-    return 'You win! Scissors beats paper.'
+// function gamePlay (playerSelection, getComputerChoice) {
+//     if (playerSelection.toLowerCase === "rock" && getComputerChoice=== "Paper")
+//         return 'You lose! Paper beats rock.'
 
-    else (playerSelection === "paper" && getComputerChoice=== "Paper")
-    return 'It\'s a tie.'
-}
+//     if (playerSelection.toLowerCase === "scissors" && getComputerChoice=== "Paper")
+//     return 'You win! Scissors beats paper.'
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(gamePlay(playerSelection, getComputerChoice));
+//     if (playerSelection.toLowerCase === "paper" && getComputerChoice=== "Paper")
+//     return 'It\'s a tie.'
+
+//     if (playerSelection.toLowerCase === "rock" && getComputerChoice=== "Rock")
+//         return 'It is a tie.'
+
+//     if (playerSelection.toLowerCase === "scissors" && getComputerChoice=== "Rock")
+//     return 'You lose! Rock beats scissors.'
+
+//     if (playerSelection.toLowerCase === "paper" && getComputerChoice=== "Rock")
+//     return 'You win! Paper beats rock.'
+
+//     if (playerSelection.toLowerCase === "rock" && getComputerChoice=== "Scissors")
+//         return 'You win! Rock beats scissors.'
+
+//     if (playerSelection.toLowerCase === "scissors" && getComputerChoice=== "Scissors")
+//     return 'Tie game.'
+
+//     if (playerSelection.toLowerCase === "paper" && getComputerChoice=== "Scissors")
+//     return 'You lose! Scissors beats paper.'
+
+//     else return 'Try again'
+// }
+
+// const playerSelection = "scissors"
+// const computerSelection = getComputerChoice();
+// console.log(gamePlay(playerSelection, getComputerChoice));
